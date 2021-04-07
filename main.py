e3a48c2e4775a7d8a95ff2235944936de626d757
SirGlory <gregch4@gmail.com>
2021-04-07 11:23:54 +0200

    Initial Commit

---
+++ main.py
@@ -0,0 +1,25 @@
+import inspect
+import justpy as jp
+from definition import Definition
+from webapp import page
+from webapp.home import Home
+from webapp.about import About
+from webapp.dictionary import Dictionary
+
+imports = list(globals().values())
+
+for obj in imports:
+    if inspect.isclass(obj):
+        if issubclass(obj, page.Page) and obj is not page.Page:
+            jp.Route(obj.path, obj.serve)
+
+
+#jp.Route(Home.path, Home.serve)
+#jp.Route(About.path, About.serve)
+#jp.Route(Dictionary.path, Dictionary.serve)
+
+#term = 'sun'
+#d = Definition(term=term)
+#print(d.get())
+
+jp.justpy(port=8001)
